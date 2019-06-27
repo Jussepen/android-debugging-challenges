76f932e76a9b8008f30545848f47bd5356e0cf54
ajin12 <amy_jin@college.harvard.edu>
2019-06-27 13:36:57 -0700

    solved 4 milestones

--- app/src/main/java/com/codepath/debuggingchallenges/adapters/MoviesAdapter.java
+++ app/src/main/java/com/codepath/debuggingchallenges/adapters/MoviesAdapter.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -47,7 +46,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movies.size();
     }
 
     @NonNull
@@ -75,8 +74,10 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
         Resources resources = viewHolder.tvName.getResources();
         double movieRating = movie.getRating();
 
-        if (movieRating > 6) {
+        if (movieRating > 7) {
             viewHolder.view.setBackgroundColor(Color.GREEN);
+        } else {
+            viewHolder.view.setBackgroundColor(Color.WHITE);
         }
 
         String ratingText = String.format(resources.getString(R.string.rating), movieRating);
